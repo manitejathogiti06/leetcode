60283a8663147c29227ead7e7e973e2a9deca37f
manitejathogiti06 <manitejathogiti06@gmail.com>
2026-07-21 22:42:57 +0530

    Time: 2 ms (42.28%), Space: 42.8 MB (76.39%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,31 +1,25 @@
 class Solution {
     public int minAddToMakeValid(String s) {
 
-        int count = 0;
-
         Stack<Character> stack = new Stack<>();
 
         for(char ch : s.toCharArray()){
 
-            if(ch == '(' || ch == '{' || ch == '['){
+            if (ch == '('){
 
                 stack.push(ch);
-            } else{
+            }else{
 
-                if(!stack.isEmpty() && stack.peek() == '('){
-                   
+                if(!stack.isEmpty() && stack.peek() =='('){
 
-                   stack.pop();
+                    stack.pop();
                 } else{
 
                     stack.push(ch);
-
-
                 }
             }
         }
         return stack.size();
+        
     }
 }
-
-                
